dbe98aeb814cdf0f2675ce6eaeff8e2b2366f1b8
militrik <militrik@gmail.com>
2023-04-22 12:42:20 +0300

    [+] add Actions

--- MenuHD44780.cpp
+++ MenuHD44780.cpp
@@ -2,39 +2,71 @@
 #include <cstring>
 #include <algorithm>
 
-char MenuHD44780::displayField[HD44780_ROWS][HD44780_COLUMNS+1] = {0};
-
+char MenuHD44780::displayField[HD44780_ROWS][HD44780_COLUMNS + 1] = {0};
 MenuHD44780::MenuHD44780() = default;
+MenuHD44780 * MenuHD44780::menuPtr = nullptr;
 
-void MenuHD44780::setMenuPtr(MenuHD44780 *) {}
+void MenuHD44780::setMenuPtr(MenuHD44780 *menuptr) {
+    MenuHD44780::menuPtr = menuptr;
+}
 
-void MenuHD44780::createItem(const char *formatter, void *varPtr, VarType varType, uint8_t rowPos, uint8_t colPos) {
+MenuHD44780 *MenuHD44780::getMenuPtr() {
+    return menuPtr;
+}
+
+void MenuHD44780::createItem(const char *formatter, void *varPtr, VarType varType,  uint8_t rowPos, uint8_t colPos) {
     Item item = {formatter, varPtr, varType, rowPos, colPos};
     items.push_back(item);
 }
 
 void MenuHD44780::renewAll() {
+#include <cstring> // For memset
+    memset(displayField, 0, sizeof(displayField));
     char row[HD44780_COLUMNS + 1];
     for (Item item: items) {
         switch (item.varType) {
             case STRTYPE:
-                sprintf(row, item.format, (char *) item.varPtr);
+                snprintf(row, sizeof row, item.format, (char *) item.varPtr);
                 break;
             case INTTYPE:
-                sprintf(row, item.format, *(int *) item.varPtr);
+                snprintf(row, sizeof row, item.format, *(uint8_t *) item.varPtr);
                 break;
             case FLOATTYPE:
-                sprintf(row, item.format, *(float *) item.varPtr);
+                snprintf(row, sizeof row, item.format, *(float *) item.varPtr);
                 break;
         }
         strncpy(&MenuHD44780::displayField[item.rowPos][item.colPos], row, strlen(row));
     }
-    for (auto & i : MenuHD44780::displayField) {
-        std::replace(&i[0], &i[0] + HD44780_COLUMNS, '\0', ' ');
+//    for (uint8_t i = 0; i < HD44780_ROWS; ++i) {
+//        std::replace(&MenuHD44780::displayField[i][0], &MenuHD44780::displayField[i][0] + HD44780_COLUMNS, '\0', ' ');
+//    }
+    for (auto &i: MenuHD44780::displayField) {
+        std::replace(i, i + HD44780_COLUMNS, '\0', ' ');
     }
 }
 
-char* MenuHD44780::getDisplayField(uint8_t row) {
+char *MenuHD44780::getDisplayField(uint8_t row) {
     return &MenuHD44780::displayField[row][0];
 //    return 0;
 }
+
+MenuHD44780::MenuHD44780(MenuHD44780 *enterPtr, MenuHD44780 *escPtr, MenuHD44780 *leftPtr,
+                         MenuHD44780 *rightPtr) : enterPtr(enterPtr), escPtr(escPtr),
+                                                     leftPtr(leftPtr), rightPtr(rightPtr) {}
+
+void MenuHD44780::enterAction() {
+    menuPtr = this->enterPtr;
+}
+
+void MenuHD44780::escAction() {
+    menuPtr = this->escPtr;
+}
+
+void MenuHD44780::leftAction() {
+    menuPtr = this->leftPtr;
+}
+
+void MenuHD44780::rightAction() {
+    menuPtr = this->rightPtr;
+}
+
--- MenuHD44780.hpp
+++ MenuHD44780.hpp
@@ -25,23 +25,32 @@ struct Item {
     uint8_t colPos;
 };
 
+//onFunc;
+
 class MenuHD44780 {
 public:
     static MenuHD44780 *menuPtr;
 
-
     MenuHD44780();
-
+    MenuHD44780(MenuHD44780 *, MenuHD44780 *, MenuHD44780 *, MenuHD44780 *);
     static void setMenuPtr(MenuHD44780 *);
-
+    static char *getDisplayField(uint8_t);
+    static MenuHD44780 *getMenuPtr();
     void createItem(const char *formatter, void *varPtr, VarType varType, uint8_t rowPos, uint8_t colPos);
-
     void renewAll();
+    void enterAction();
+    void escAction();
+    void leftAction();
+    void rightAction();
 
-    static char* getDisplayField(uint8_t);
 
 private:
-    static char displayField[HD44780_ROWS][HD44780_COLUMNS+1];
+    static char displayField[HD44780_ROWS][HD44780_COLUMNS + 1];
+
+    MenuHD44780 * enterPtr;
+    MenuHD44780 * escPtr;
+    MenuHD44780 * leftPtr;
+    MenuHD44780 * rightPtr;
     std::vector<Item> items;
 };
 
